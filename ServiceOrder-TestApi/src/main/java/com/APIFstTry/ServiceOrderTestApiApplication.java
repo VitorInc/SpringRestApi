03eb7b94199b323419479095e26bcc91bfa3c8a6
VitorInc <vitordc.contato@gmail.com>
2021-03-13 17:35:17 -0300

    Starting project

---
+++ ServiceOrder-TestApi/src/main/java/com/APIFstTry/ServiceOrderTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.APIFstTry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderTestApiApplication.class, args);
+	}
+
+}
