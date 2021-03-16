4102d8092e9613d9299c209e914f4d5c8fff4b66
VitorInc <vitordc.contato@gmail.com>
2021-03-16 17:06:16 -0300

    JPA

--- ServiceOrder-TestApi/src/main/java/com/APIFstTry/api/controller/ClientController.java
+++ ServiceOrder-TestApi/src/main/java/com/APIFstTry/api/controller/ClientController.java
@@ -1,36 +1,25 @@
 package com.APIFstTry.api.controller;
 
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.APIFstTry.domain.model.Clients;
-
-
-
+import com.APIFstTry.domain.model.Client;
 
 @RestController
 public class ClientController {
+	@PersistenceContext
+	private EntityManager manager;
 	
-	@GetMapping("/clients")
-	public List<Clients> listar() {
-		var cliente1 = new Clients();
-		cliente1.setId(1l);
-		cliente1.setName("João");
-		cliente1.setEmail("Joao@email");
-		cliente1.setTelephone("+55 (11) 9 9874-1234");
-		
-		
-		var cliente2 = new Clients();
-		cliente2.setId(2l);
-		cliente2.setName("Ana");
-		cliente2.setEmail("ana@email");
-		cliente2.setTelephone("+55 (11) 9 1234-9874");
-		
-		return Arrays.asList(cliente1,cliente2);
+	@GetMapping("/clientes")
+	public List<Client> listar() {
+		return manager.createQuery("from Client", Client.class)
+				.getResultList();
 	}
 
 }
