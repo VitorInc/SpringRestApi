d91b646d446cfd7e270e472afb03fa92aac95ec2
VitorInc <vitordc.contato@gmail.com>
2021-03-17 08:09:32 -0300

    just wrote get post put delete methods

--- ServiceOrder-TestApi/src/main/java/com/APIFstTry/api/controller/ClientController.java
+++ ServiceOrder-TestApi/src/main/java/com/APIFstTry/api/controller/ClientController.java
@@ -2,24 +2,59 @@ package com.APIFstTry.api.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.APIFstTry.domain.model.Client;
+import com.APIFstTry.domain.repository.ClientRepository;
 
 @RestController
 public class ClientController {
-	@PersistenceContext
-	private EntityManager manager;
+	
+	@Autowired
+	private ClientRepository clientRepository;
 	
 	@GetMapping("/clientes")
-	public List<Client> listar() {
-		return manager.createQuery("from Client", Client.class)
-				.getResultList();
+	public List <Client> listar(){
+		return clientRepository.findAll();
 	}
-
+	@GetMapping("/{clientId}")
+	public ResponseEntity <Client> buscar(@PathVariable Long clientId) {
+	Optional<Client> client  = clientRepository.findById(clientId);
+	
+	if(client.isPresent()) {
+		return ResponseEntity.ok(client.get());
+	}
+	return ResponseEntity.notFound().build();
+	}
+	@PostMapping
+	public Client adicionar(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	@PutMapping("{clientId}")
+	public ResponseEntity<Client> atualizar(@PathVariable Long clientId,
+		@RequestBody Client client ){
+		
+		if(clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		client.setId(clientId);
+		clientRepository.save(client);
+		return ResponseEntity.ok(client);
+	}
+	
 }
